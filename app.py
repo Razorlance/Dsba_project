fce5ac945cc2879feb7dafcb2c899a5648927632
Denis Kulakov <8269905@gmail.com>
2021-11-29 02:56:44 +0300

    Added card template
    * Added new calculations

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    print(min(map(int, input().split()), key=lambda x: (not (x % 2), x)))
     return render_template("index.html")
 
 

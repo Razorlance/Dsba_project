a05ec4a43ed2b54b67cf41635c0d236bdcff3a4b
Denis Kulakov <8269905@gmail.com>
2021-12-04 19:09:51 +0300

    Added layout sample

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/template')
+def template_index():
+    return render_template("template.html")
+
+
 if __name__ == '__main__':
     app.run()

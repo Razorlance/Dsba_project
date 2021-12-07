6132c9fede9f2533ef7f76f7f9d92036f582994b
Denis Kulakov <8269905@gmail.com>
2021-12-07 18:37:37 +0300

    Added templates for other pages
    * Added new pie chart
    * Changed directory of js

---
+++ static/Scripts/Netflix.js
@@ -0,0 +1,55 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const configLine = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const configPie = {
+    type: 'pie',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Pie Chart'
+            }
+        }
+    },
+};
+const PieChart = new Chart(
+    document.getElementById('PieChart'),
+    configPie
+);
+const LineChart = new Chart(
+    document.getElementById('LineChart'),
+    configLine
+);

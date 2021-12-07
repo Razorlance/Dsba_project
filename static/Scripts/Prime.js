871267bdb35e2b50926eed9e6ed9e0996d7ca4f0
Denis Kulakov <8269905@gmail.com>
2021-12-07 20:54:28 +0300

    Added new colors and templates

---
+++ static/Scripts/Prime.js
@@ -0,0 +1,111 @@
+const month_labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+const movie_labels = [
+    "Horror",
+    "Action",
+    "Comedy"
+]
+
+
+const line_data = {
+    labels: month_labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+const pie_data = {
+    labels: movie_labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const configLine = {
+    type: 'line',
+    data: line_data,
+    options: {}
+};
+
+const configPie = {
+    type: 'pie',
+    data: pie_data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+            }
+        }
+    },
+};
+
+const bar_data = {
+    labels: month_labels,
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+            'rgba(255, 99, 132)',
+            'rgba(255, 159, 64)',
+            'rgba(255, 205, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(54, 162, 235)',
+            'rgba(153, 102, 255)',
+            'rgba(201, 203, 207)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+};
+const configBar = {
+    type: 'bar',
+    data: bar_data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    },
+};
+const PieChart = new Chart(
+    document.getElementById('PieChart'),
+    configPie
+);
+const LineChart = new Chart(
+    document.getElementById('LineChart'),
+    configLine
+);
+const BarChart = new Chart(
+    document.getElementById('BarChart'),
+    configBar
+);
